8217a9869db2f9a9f3a1dbad84a7c014cfffd577
ahmed <ahmed@DELL>
2024-02-25 18:19:48 +0100

    Ajoutez des fichiers projet.

---
+++ Verzon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Verzon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
